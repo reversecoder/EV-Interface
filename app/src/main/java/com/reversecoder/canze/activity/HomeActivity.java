84ccf82714d0218536a2787f1ed9f52d4210ca8c
Md. Rashadul Alam <rashedul.alam@bjitgroup.com>
2017-08-11 11:28:16 +0600

    rc-canze-demo: added session timeout function

--- app/src/main/java/com/reversecoder/canze/activity/HomeActivity.java
+++ app/src/main/java/com/reversecoder/canze/activity/HomeActivity.java
@@ -1,14 +1,20 @@
 package com.reversecoder.canze.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.reversecoder.canze.R;
+import com.reversecoder.library.storage.SessionManager;
+import com.reversecoder.sessiontimeout.engine.activity.SessionTimeoutAppCompatActivity;
+import com.reversecoder.sessiontimeout.engine.bearing.SessionTimeoutDialogCallback;
+import com.reversecoder.sessiontimeout.engine.injector.SessionTimeoutManager;
 
-public class HomeActivity extends AppCompatActivity {
+import static com.reversecoder.canze.util.AllConstants.IS_USER_LOGGED_IN;
+
+public class HomeActivity extends SessionTimeoutAppCompatActivity {
 
     RelativeLayout relativeLayoutItemDashboard, relativeLayoutItemMap, relativeLayoutItemSettings;
 
@@ -16,17 +22,28 @@ public class HomeActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        initLoginUI();
-        initLoginAction();
+        initHomeUI();
+        initHomeAction();
     }
 
-    private void initLoginUI() {
+    private void initHomeUI() {
         relativeLayoutItemDashboard = (RelativeLayout) findViewById(R.id.item_dashboard);
         relativeLayoutItemMap = (RelativeLayout) findViewById(R.id.item_map);
         relativeLayoutItemSettings = (RelativeLayout) findViewById(R.id.item_settings);
     }
 
-    private void initLoginAction() {
+    private void initHomeAction() {
+
+        SessionTimeoutManager.initSessionTimeoutManager(new SessionTimeoutDialogCallback() {
+            @Override
+            public void sessionTimeoutButtonClick(DialogInterface dialog) {
+                dialog.dismiss();
+
+                doSignOut();
+            }
+        });
+        SessionTimeoutManager.startSessionTimeoutTask(5 * 60 * 1000);
+
         relativeLayoutItemDashboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,5 +64,12 @@ public class HomeActivity extends AppCompatActivity {
                 startActivity(new Intent(HomeActivity.this, SettingsActivity.class));
             }
         });
+    }
+
+    private void doSignOut() {
+        SessionManager.setBooleanSetting(HomeActivity.this, IS_USER_LOGGED_IN, false);
+        Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
